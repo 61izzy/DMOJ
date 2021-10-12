b898347348d81379ae19449bd928144cf4a5f6a7
61izzy <89612466+61izzy@users.noreply.github.com>
2021-10-12 16:34:31 -0400

    Update ccc14s2.java

--- ccc14s2.java
+++ ccc14s2.java
@@ -6,15 +6,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         
         int n = scanner.nextInt();
-        String[] arr = new String[n];
+        scanner.nextLine();
+        String[] arr = scanner.nextLine().split(" ");
         HashMap<String, String> partner = new HashMap<String, String>();
         boolean match = true;
         
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.next().toLowerCase();
-        }
-        for (int i = 0; i < n; i++) {
-            partner.put(arr[i], scanner.next().toLowerCase());
+            partner.put(arr[i], scanner.next());
         }
         for (int i = 0; i < n; i++) {
             if (!partner.get(partner.get(arr[i])).equals(arr[i]) || partner.get(arr[i]).equals(arr[i])) {

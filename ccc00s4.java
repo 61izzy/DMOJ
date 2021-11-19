ee1777cae1258a662514d8338c3f8b78898c9161
61izzy <89612466+61izzy@users.noreply.github.com>
2021-11-18 20:15:19 -0500

    Update ccc00s4.java

--- ccc00s4.java
+++ ccc00s4.java
@@ -5,20 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int d = scanner.nextInt(), n = scanner.nextInt(), v[] = new int[n + 1], dp[] = new int[d + 1];
+        int d = scanner.nextInt(), n = scanner.nextInt(), dp[] = new int[d + 1];
 
         for (int i = 1; i <= d; i++) {
             dp[i] = 10000;
         }
-
-        for (int i = 1; i <= n; i++) {
-            v[i] = scanner.nextInt();
-        }
         
         for (int i = 1; i <= n; i++) {
+            int v = scanner.nextInt();
             for (int j = 1; j <= d; j++) {
-                if (v[i] <= j) {
-                    dp[j] = Math.min(dp[j], 1 + dp[j - v[i]]);
+                if (v <= j) {
+                    dp[j] = Math.min(dp[j], 1 + dp[j - v]);
                 }
             }
         }

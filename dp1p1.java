e2f8e87e988558d8ee1fd3074c6b821412d9a23b
61izzy <89612466+61izzy@users.noreply.github.com>
2021-12-01 17:27:04 -0500

    Update dp1p1.java

--- dp1p1.java
+++ dp1p1.java
@@ -6,18 +6,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         
         int n = scanner.nextInt();
-        int[][] dp = new int[n + 1][2];
-        
-        for (int i = 1; i <= n; i++) {
-            int x = scanner.nextInt();
-            for (int j = 0; j < 2; j++) {
-                dp[i][j] = dp[i - 1][0];
-                if (j == 0) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][1] + x);
-                }
-            }
+        int[] dp = new int[n + 2];
+
+        for (int i = 2; i < n + 2; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + scanner.nextInt());
         }
-        
-        System.out.println(Math.max(dp[n][0], dp[n][1]));
+
+        System.out.println(dp[n + 1]);
     }
 }

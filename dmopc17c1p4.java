2827093c3a7e8e304ab0f42ffee39e4b9ae45c69
61izzy <89612466+61izzy@users.noreply.github.com>
2021-12-17 13:01:34 -0500

    Update dmopc17c1p4.java

--- dmopc17c1p4.java
+++ dmopc17c1p4.java
@@ -10,15 +10,11 @@ public class Main {
         
         for (int i = 0; i < n; i++) {
             int g = scanner.nextInt(), h = scanner.nextInt(), q = scanner.nextInt(), t = scanner.nextInt();
-            boolean[] vis = new boolean[c + 1];
             for (int j = h; j <= c; j++) {
                 dp[j][1] = g + dp[j - h][0];
-                vis[j] = true;
             }
             for (int j = h + t; j <= c; j++) {
-                if (vis[j - t]) {
-                    dp[j][1] = Math.max(dp[j][1], q + dp[j - t][1]);
-                }
+                dp[j][1] = Math.max(dp[j][1], q + dp[j - t][1]);
             }
             for (int j = h; j <= c; j++) {
                 dp[j][0] = Math.max(dp[j][0], dp[j][1]);

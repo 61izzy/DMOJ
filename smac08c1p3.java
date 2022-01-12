26d6c545368cf7c8c69b773e8fcfa8c97cbe9d8a
61izzy <89612466+61izzy@users.noreply.github.com>
2022-01-12 10:58:26 -0500

    Update smac08c1p3.java

--- smac08c1p3.java
+++ smac08c1p3.java
@@ -6,19 +6,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int n = Integer.parseInt(st.nextToken()), d = Integer.parseInt(st.nextToken()), dist[] = new int[n], tree[] = new int[2 * d];
+        int n = Integer.parseInt(st.nextToken()), d = Integer.parseInt(st.nextToken()), dist = 0, tree[] = new int[2 * d];
         Arrays.fill(tree, Integer.MAX_VALUE);
         tree[1] = 0;
 
         for (int i = 0; i < n; i++) {
-            dist[i] = Integer.parseInt(br.readLine()) + tree[1];
+            dist = Integer.parseInt(br.readLine()) + tree[1];
             int idx = i%d + d;
-            tree[idx] = dist[i];
+            tree[idx] = dist;
             for (idx /= 2; idx > 0; idx /= 2) {
                 tree[idx] = Math.min(tree[idx * 2], tree[idx * 2 + 1]);
             }
         }
 
-        System.out.println(dist[n - 1]);
+        System.out.println(dist);
     }
 }

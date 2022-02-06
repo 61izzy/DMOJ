02d7e9ea1c72408d24f444f5ca02a0e717963400
61izzy <89612466+61izzy@users.noreply.github.com>
2022-02-05 23:59:41 -0500

    Update coci09c7p4.java

--- coci09c7p4.java
+++ coci09c7p4.java
@@ -8,7 +8,7 @@ public class Main {
         StringTokenizer st;
         
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Point> pqx = new PriorityQueue<Point>(n, (a, b) -> a.x - b.x), pqy = new PriorityQueue<Point>(n, (a, b) -> a.y - b.y), pqz = new PriorityQueue<Point>(n, (a, b) -> a.z - b.z);
+        PriorityQueue<Point> pqx = new PriorityQueue<Point>(n, (a, b) -> a.c - b.c), pqy = new PriorityQueue<Point>(n, (a, b) -> a.c - b.c), pqz = new PriorityQueue<Point>(n, (a, b) -> a.c - b.c);
         id = new int[n];
         ArrayList<Edge> edges = new ArrayList<Edge>();
         long c = 0;
@@ -17,15 +17,15 @@ public class Main {
             id[i] = -1;
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken()), z = Integer.parseInt(st.nextToken());
-            pqx.offer(new Point(i, x, y, z));
-            pqy.offer(new Point(i, x, y, z));
-            pqz.offer(new Point(i, x, y, z));
+            pqx.offer(new Point(i, x));
+            pqy.offer(new Point(i, y));
+            pqz.offer(new Point(i, z));
         }
         for (int i = 1; i < n; i++) {
             Point px = pqx.poll(), cx = pqx.peek(), py = pqy.poll(), cy = pqy.peek(), pz = pqz.poll(), cz = pqz.peek();
-            edges.add(new Edge(px.idx, cx.idx, cx.x - px.x));
-            edges.add(new Edge(py.idx, cy.idx, cy.y - py.y));
-            edges.add(new Edge(pz.idx, cz.idx, cz.z - pz.z));
+            edges.add(new Edge(px.i, cx.i, cx.c - px.c));
+            edges.add(new Edge(py.i, cy.i, cy.c - py.c));
+            edges.add(new Edge(pz.i, cz.i, cz.c - pz.c));
         }
         
         Collections.sort(edges, (a, b) -> a.c - b.c);
@@ -55,12 +55,10 @@ public class Main {
 }
 
 class Point {
-    int idx, x, y, z;
-    public Point(int idx, int x, int y, int z) {
-        this.idx = idx;
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    int i, c;
+    public Point(int i, int c) {
+        this.i = i;
+        this.c = c;
     }
 }
 

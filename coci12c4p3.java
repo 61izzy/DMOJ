39b80ef5833a9378b55b5866238d9086a3317191
61izzy <89612466+61izzy@users.noreply.github.com>
2022-02-21 17:28:01 -0500

    Update coci12c4p3.java

--- coci12c4p3.java
+++ coci12c4p3.java
@@ -13,44 +13,42 @@ public class Main {
         Queue<Signal> q = new LinkedList<Signal>();
         char[] arr = {'U', 'R', 'D', 'L'};
         int[] dy = {0, 1, 0, -1}, dx = {-1, 0, 1, 0};
-        HashMap<Character, Integer> dir = new HashMap<Character, Integer>();
         for (int i = 0; i < 4; i++) {
-            dir.put(arr[i], i);
             if (x + dx[i] >= 0 && x + dx[i] < n && y + dy[i] >= 0 && y + dy[i] < m && grid[x + dx[i]].charAt(y + dy[i]) != 'C') {
-                q.offer(new Signal(x + dx[i], y + dy[i], 2, arr[i], arr[i]));
+                q.offer(new Signal(x + dx[i], y + dy[i], 2, i, arr[i]));
             }
         }
         while (!q.isEmpty()) {
             Signal curr = q.poll();
-            if (curr.x == x && curr.y == y && curr.d == curr.s) {
+            if (curr.x == x && curr.y == y && arr[curr.d] == curr.s) {
                 res = Integer.MAX_VALUE;
                 d = curr.s;
                 break;
             }
             else if (grid[curr.x].charAt(curr.y) == '\\' || grid[curr.x].charAt(curr.y) == '/') {
-                if ((curr.d == 'L' && grid[curr.x].charAt(curr.y) == '\\' || curr.d == 'R' && grid[curr.x].charAt(curr.y) == '/') && curr.x > 0 && grid[curr.x - 1].charAt(curr.y) != 'C') {
-                    q.offer(new Signal(curr.x - 1, curr.y, curr.t + 1, 'U', curr.s));
+                if ((curr.d == 3 && grid[curr.x].charAt(curr.y) == '\\' || curr.d == 1 && grid[curr.x].charAt(curr.y) == '/') && curr.x > 0 && grid[curr.x - 1].charAt(curr.y) != 'C') {
+                    q.offer(new Signal(curr.x - 1, curr.y, curr.t + 1, 0, curr.s));
                     if (curr.t + 1 > res) {
                         res = curr.t + 1;
                         d = curr.s;
                     }
                 }
-                else if ((curr.d == 'L' && grid[curr.x].charAt(curr.y) == '/' || curr.d == 'R' && grid[curr.x].charAt(curr.y) == '\\') && curr.x < n - 1 && grid[curr.x + 1].charAt(curr.y) != 'C') {
-                    q.offer(new Signal(curr.x + 1, curr.y, curr.t + 1, 'D', curr.s));
+                else if ((curr.d == 3 && grid[curr.x].charAt(curr.y) == '/' || curr.d == 1 && grid[curr.x].charAt(curr.y) == '\\') && curr.x < n - 1 && grid[curr.x + 1].charAt(curr.y) != 'C') {
+                    q.offer(new Signal(curr.x + 1, curr.y, curr.t + 1, 2, curr.s));
                     if (curr.t + 1 > res) {
                         res = curr.t + 1;
                         d = curr.s;
                     }
                 }
-                else if ((curr.d == 'D' && grid[curr.x].charAt(curr.y) == '\\' || curr.d == 'U' && grid[curr.x].charAt(curr.y) == '/') && curr.y < m - 1 && grid[curr.x].charAt(curr.y + 1) != 'C') {
-                    q.offer(new Signal(curr.x, curr.y + 1, curr.t + 1, 'R', curr.s));
+                else if ((curr.d == 2 && grid[curr.x].charAt(curr.y) == '\\' || curr.d == 0 && grid[curr.x].charAt(curr.y) == '/') && curr.y < m - 1 && grid[curr.x].charAt(curr.y + 1) != 'C') {
+                    q.offer(new Signal(curr.x, curr.y + 1, curr.t + 1, 1, curr.s));
                     if (curr.t + 1 > res) {
                         res = curr.t + 1;
                         d = curr.s;
                     }
                 }
-                else if ((curr.d == 'D' && grid[curr.x].charAt(curr.y) == '/' || curr.d == 'U' && grid[curr.x].charAt(curr.y) == '\\') && curr.y > 0 && grid[curr.x].charAt(curr.y - 1) != 'C') {
-                    q.offer(new Signal(curr.x, curr.y - 1, curr.t + 1, 'L', curr.s));
+                else if ((curr.d == 2 && grid[curr.x].charAt(curr.y) == '/' || curr.d == 0 && grid[curr.x].charAt(curr.y) == '\\') && curr.y > 0 && grid[curr.x].charAt(curr.y - 1) != 'C') {
+                    q.offer(new Signal(curr.x, curr.y - 1, curr.t + 1, 3, curr.s));
                     if (curr.t + 1 > res) {
                         res = curr.t + 1;
                         d = curr.s;
@@ -58,8 +56,8 @@ public class Main {
                 }
             }
             else {
-                if (curr.x + dx[dir.get(curr.d)] >= 0 && curr.x + dx[dir.get(curr.d)] < n && curr.y + dy[dir.get(curr.d)] >= 0 && curr.y + dy[dir.get(curr.d)] < m && grid[curr.x + dx[dir.get(curr.d)]].charAt(curr.y + dy[dir.get(curr.d)]) != 'C') {
-                    q.offer(new Signal(curr.x + dx[dir.get(curr.d)], curr.y + dy[dir.get(curr.d)], curr.t + 1, curr.d, curr.s));
+                if (curr.x + dx[curr.d] >= 0 && curr.x + dx[curr.d] < n && curr.y + dy[curr.d] >= 0 && curr.y + dy[curr.d] < m && grid[curr.x + dx[curr.d]].charAt(curr.y + dy[curr.d]) != 'C') {
+                    q.offer(new Signal(curr.x + dx[curr.d], curr.y + dy[curr.d], curr.t + 1, curr.d, curr.s));
                     if (curr.t + 1 > res) {
                         res = curr.t + 1;
                         d = curr.s;
@@ -73,9 +71,9 @@ public class Main {
 }
 
 class Signal {
-    int x, y, t;
-    char d, s;
-    public Signal(int x, int y, int t, char d, char s) {
+    int x, y, t, d;
+    char s;
+    public Signal(int x, int y, int t, int d, char s) {
         this.x = x;
         this.y = y;
         this.t = t;
@@ -86,3 +84,4 @@ class Signal {
 
 // this really took 1 hour to debug... -_-
 // could've made it a lot simpler too .-.
+// edit: removed the hashmap (why tf was i using a hashmap???)
